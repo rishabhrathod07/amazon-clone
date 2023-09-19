74d4f6c8e3939bff0317dd83ff385f7191fc41f5
rishabhrathod07 <108474745+rishabhrathod07@users.noreply.github.com>
2023-09-19 18:53:46 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("./db/conn");
+const cookieParser = require("cookie-parser");
+
+const Products = require("./models/productsSchema");
+const DefaultData = require("./defaultdata");
+const cors = require("cors"); 
+const router = require("./routes/router");
+
+app.use(express.json());
+app.use(cookieParser(""));
+app.use(cors());
+
+app.use(router);
+
+const port = process.env.PORT || 8005;
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("amazon-clone/build"));
+}
+
+app.listen(port,()=>{
+    console.log(`your server is running on port ${port} `);
+});
+
+
+DefaultData();
